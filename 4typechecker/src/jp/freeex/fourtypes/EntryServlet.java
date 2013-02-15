ca05be4f3f0c58100a02e2de97b923cb16380f2f
4typechecker.ex <4typechecker.ex@gmail.com>
2013-02-15 09:40:14 +0000

    [2013/02/15] hori DDoS対策のアクセス回数制限をServletFilter化

--- 4typechecker/src/jp/freeex/fourtypes/EntryServlet.java
+++ 4typechecker/src/jp/freeex/fourtypes/EntryServlet.java
@@ -1,49 +1,36 @@
 ﻿package jp.freeex.fourtypes;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class EntryServlet extends BaseServlet {
 
 	private static final long serialVersionUID = -1141919162647029771L;
-	private Logger log = Logger.getLogger(getClass().getName());
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp){
 		try{
-			// DDos対策
-			if(isHeavyUser(req)){
-				sendRedirect(JSP_HEAVYUSER, req, resp);
-			}else{
-				// サブタイトルなしで
-				req.setAttribute(REQ_ATTRKEY_SUBTITLE, " ");
-				// ユーザ情報を作成し、Attributeとして格納
-				req.setAttribute(REQ_ATTRKEY_USER, getUser(req));
-				sendRedirect(JSP_ENTRY, req, resp);
-			}
+
+			// サブタイトルなしで
+			req.setAttribute(REQ_ATTRKEY_SUBTITLE, " ");
+			// ユーザ情報を作成し、Attributeとして格納
+			req.setAttribute(REQ_ATTRKEY_USER, getUser(req));
+			sendRedirect(JSP_ENTRY, req, resp);
 		}catch(Exception e){
-			log.log(Level.INFO, "EntryServlet#doGet()処理中例外", e);
+			info(getClass().getName(), "EntryServlet#doGet()処理中例外", e);
 			sendRedirect(JSP_ERROR, req, resp);
 		}
 	}
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp){
 		try{
-			// DDos対策
-			if(isHeavyUser(req)){
-				sendRedirect(JSP_HEAVYUSER, req, resp);
-			}else{
-				// サブタイトルなしで
-				req.setAttribute(REQ_ATTRKEY_SUBTITLE, " ");
-			
-				// ユーザ情報を作成し、Attributeとして格納
-				req.setAttribute(REQ_ATTRKEY_USER, getUser(req));
-				sendRedirect(JSP_ENTRY, req, resp);
-			}
+			// サブタイトルなしで
+			req.setAttribute(REQ_ATTRKEY_SUBTITLE, " ");
+
+			// ユーザ情報を作成し、Attributeとして格納
+			req.setAttribute(REQ_ATTRKEY_USER, getUser(req));
+			sendRedirect(JSP_ENTRY, req, resp);
 		}catch(Exception e){
-			log.log(Level.INFO, "EntryServlet#doPost()処理中例外", e);
+			info(getClass().getName(), "EntryServlet#doPost()処理中例外", e);
 			sendRedirect(JSP_ERROR, req, resp);
 		}
 	}
--- 4typechecker/src/jp/freeex/fourtypes/StatusServlet.java
+++ 4typechecker/src/jp/freeex/fourtypes/StatusServlet.java
@@ -1,7 +1,5 @@
 ﻿package jp.freeex.fourtypes;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,34 +10,23 @@ import jp.freeex.fourtypes.store.BigTableStoreManager;
 public class StatusServlet extends BaseServlet implements ViewConstants{
 
 	private static final long serialVersionUID = 6713413426700444993L;
-	private Logger log = Logger.getLogger(getClass().getName());
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp){
 		try{
-			// DDos対策
-			if(isHeavyUser(req)){
-				sendRedirect(JSP_HEAVYUSER, req, resp);
-			}else{
-				BigTableStoreManager manager = 
-						BigTableStoreManager.getInstance();
-				Count c = manager.getCount();
+			BigTableStoreManager manager = 
+					BigTableStoreManager.getInstance();
+			Count c = manager.getCount();
 
-				req.setAttribute(REQ_KEY_COUNT, c);
+			req.setAttribute(REQ_KEY_COUNT, c);
 
-				sendRedirect(JSP_STATUS, req, resp);
-			}
+			sendRedirect(JSP_STATUS, req, resp);
 		}catch(Exception e){
-			log.log(Level.INFO, "StatusServlet実行中の例外", e);
+			info(getClass().getName(), "StatusServlet実行中の例外", e);
 			sendRedirect(JSP_ERROR, req, resp);
 		}
 	}
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp){
-		// DDos対策
-		if(isHeavyUser(req)){
-			sendRedirect(JSP_HEAVYUSER, req, resp);
-		}else{
-			sendRedirect(JSP_NOLINK, req, resp);
-		}
+		sendRedirect(JSP_NOLINK, req, resp);
 	}
 }
